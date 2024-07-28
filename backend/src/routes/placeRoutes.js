c62ba79d549d7bbcfef07aca3ab5125fece3e04a
huipadyam <cekim3197@ewhain.net>
2024-07-28 23:16:39 +0900

    feat : 여행지 수정 엔드포인트 생성 on placeRoutes.js

--- backend/src/routes/placeRoutes.js
+++ backend/src/routes/placeRoutes.js
@@ -23,6 +23,26 @@ router.post('/', async (req, res) => {
     }
 });
 
+// 여행지 수정
+router.put('/:placeId', async (req, res) => {
+    try{
+        const { placeId } = req.params;
+        const { name, description, location, rating } = req.body;
 
+        const updateData = {};
+        if( name ) updateData.name = name; // req body에 일부 필드만 오더라도 수정 가능
+        if( description ) updateData.description = description;
+        if( location ) updateData.location = location;
+        if( rating ) updateData.rating = rating;
+
+        const updatedPlace = await prisma.place.update({
+            where: { id: placeId },
+            data: updateData // 중괄호 넣으면 안 됨
+        });
+        res.status(200).json(updatedPlace);
+    } catch (error){
+        res.status(400).json({ message: error.message });
+    }
+});
 
 export default router;
