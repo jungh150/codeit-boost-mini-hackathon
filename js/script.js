35ea8544ab68435becbd4be8558962a7f08c130b
hyunsun <offmymind123@gmail.com>
2024-07-29 04:43:41 +0900

    버튼이벤트 수정

--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const myPageLink = document.querySelector('.myPage');
-
-    myPageLink.addEventListener('click', function() {
-        window.location.href = '../마이페이지/index.html';
-    });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
     const searchIcon = document.getElementById('searchIcon');
     const banner = document.getElementById('banner');
-  
-    searchIcon.addEventListener('click', () => {
-      if (banner.classList.contains('show')) {
-        banner.classList.remove('show');
-      } else {
-        banner.classList.add('show');
-      }
+
+    myPageLink.addEventListener('click', function() {
+        window.location.href = '../html/mypage.html'; // 클릭 시 페이지 이동
     });
-  });
+
+    searchIcon.addEventListener('click', () => {
+        if (banner.classList.contains('show')) {
+            banner.classList.remove('show');
+        } else {
+            banner.classList.add('show');
+        }
+    });
+});
