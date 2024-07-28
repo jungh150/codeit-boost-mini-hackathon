7f271f77cab88d1ef573d8fb4d40cd5fa36f08bc
Junghwa <jungh150@naver.com>
2024-07-29 05:10:52 +0900

    내 리뷰 전체 조회, 내 리뷰 상세 조회 api 추가

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -136,6 +136,44 @@ userRouter.get('/my/wishes/:wishId', asyncHandler(async (req, res) => {
   }
 }));
 
+// 내 리뷰 전체 조회
+userRouter.get('/my/reviews', asyncHandler(async (req, res) => {
+  const myEmail = req.user.email;
+  console.log(myEmail);
+  const user = await prisma.user.findUnique({
+    where: { email: myEmail },
+    include: {
+      reviews: true,
+    },
+  });
+  if (user) {
+    res.send(user.reviews);
+  } else {
+    res.status(404).send({ message: 'Cannot find given email.' });
+  }
+}));
+
+// 내 리뷰 상세 조회
+userRouter.get('/my/reviews/:reviewId', asyncHandler(async (req, res) => {
+  const { reviewId } = req.params;
+  const myEmail = req.user.email;
+  console.log(myEmail);
+  const user = await prisma.user.findUnique({
+    where: { email: myEmail },
+    include: {
+      reviews: true,
+    },
+  });
+  const review = user.reviews.findUnique({
+    where: { id: reviewId },
+  });
+  if (review) {
+    res.send(review);
+  } else {
+    res.status(404).send({ message: 'Cannot find wish.' });
+  }
+}));
+
 // 사용자 생성
 userRouter.post('/', asyncHandler(async (req, res) => {
   const user = await prisma.user.create({
