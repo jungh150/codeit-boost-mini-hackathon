5b040028b8f9f7c9625113e2828d901dd191973f
huipadyam <cekim3197@ewhain.net>
2024-07-28 21:26:17 +0900

    create index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,26 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const placeRoutes = require('./routes/placeRoutes');
+import express from 'express';
+import placeRoutes from './routes/placeRoutes';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.port || 3000;
 
-// 미들웨어 설정
 app.use(express.json());
-
-// 라우트 설정
 app.use('/places', placeRoutes);
 
-// 데이터베이스 연결
-mongoose.connect('mongodb://localhost:27017/travel', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connected to MongoDB');
-    // 서버 시작
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}).catch((error) => {
-    console.error('Database connection error:', error);
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
 });
