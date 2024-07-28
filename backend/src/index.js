3720b800c665686db917abc2e03335d752443388
huipadyam <cekim3197@ewhain.net>
2024-07-29 01:19:41 +0900

    chore : 모든 네트워크 인터페이스에서 요청 수락하도록 변경(0.0.0.0) on index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,6 @@ const PORT = process.env.PORT || 3000; // PORT는 대문자
 app.use(express.json());
 app.use('/places', placeRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on ${PORT}`);
 });
