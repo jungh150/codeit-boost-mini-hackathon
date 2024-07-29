06d8abc70a76a10108520d68c9e2114ac822948f
huipadyam <cekim3197@ewhain.net>
2024-07-29 16:52:27 +0900

    feat : 로그인 기능 연결 및 로그인 확인 미들웨어 추가

--- backend/src/routes/placeRoutes.js
+++ backend/src/routes/placeRoutes.js
@@ -1,12 +1,48 @@
-import { Router } from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+// import { Router } from 'express';
 import { PrismaClient } from '@prisma/client';
 import asyncHandler from '../utils/asyncHandler.js'; // 1. 비동기 핸들러 래퍼 가져오기
 
-const router = Router();
 const prisma = new PrismaClient();
+const placeRouter = express.Router();
+placeRouter.use(express.json());
 
+import session from 'express-session'; // 4
+import FileStore from 'session-file-store'; // 5
+import passport from 'passport';   // 1
+
+const fileStore = FileStore(session); // 6
+const sessionSecret = process.env.SESSION_SECRET; // 7
+
+// 미들웨어 설정 // 8
+placeRouter.use(session({
+    secret: sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    store: new fileStore({ path: './sessions' }) // 세션 파일 저장 경로 설정
+  }));
+
+// 미들웨어 설정  // 2
+placeRouter.use(passport.initialize());
+placeRouter.use(passport.session());
+
+passport.serializeUser((user, done) => done(null, user)); // 9
+passport.deserializeUser((user, done) => done(null, user)); // 10
+
+// 로그인 확인 미들웨어 // 3
+const ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+      res.status(401).json({ message: 'You need to be logged in to access this resource.' });
+    }
+  };
+
+// 여행지 생성 수정 삭제 : userId도 함께 받아서 누가 생성했는지 알면 좋을텐데..
 // 여행지 생성
-router.post('/', asyncHandler(async (req, res) => { // 2. 비동기 핸들러로 감싸주기
+placeRouter.post('/', ensureAuthenticated, asyncHandler(async (req, res) => { // 2. 비동기 핸들러로 감싸주기
         const { name, description, location, rating } = req.body;
         const newPlace = await prisma.place.create({
             data: {
@@ -15,14 +51,14 @@ router.post('/', asyncHandler(async (req, res) => { // 2. 비동기 핸들러로
                 location, // latitude, longitude 포함 json
                 rating // float
             }
-        });
+        }); 
 
         res.status(201).json(newPlace);
 
 }));
 
 // 여행지 수정
-router.put('/:placeId', asyncHandler(async (req, res) => {
+placeRouter.put('/:placeId', ensureAuthenticated, asyncHandler(async (req, res) => {
         const { placeId } = req.params;
         const { name, description, location, rating } = req.body;
 
@@ -40,7 +76,7 @@ router.put('/:placeId', asyncHandler(async (req, res) => {
 }));
 
 // 여행지 삭제
-router.delete('/:placeId', asyncHandler(async (req, res) => {
+placeRouter.delete('/:placeId', ensureAuthenticated, asyncHandler(async (req, res) => {
 
         const { placeId } = req.params;
 
@@ -91,7 +127,7 @@ router.get('/:placeId', asyncHandler(async (req, res) => {
 }));
 
 // 여행지 찜 등록
-router.post('/:placeId/wish', asyncHandler(async (req, res) => {
+placeRouter.post('/:placeId/wish', ensureAuthenticated, asyncHandler(async (req, res) => {
     const { placeId } = req.params;
     const { userId, comment } = req.body;
     const newWish = await prisma.wish.create({
@@ -105,7 +141,7 @@ router.post('/:placeId/wish', asyncHandler(async (req, res) => {
 }));
 
 // 여행지 찜 삭제
-router.delete('/:placeId/wish', asyncHandler(async (req, res) => {
+placeRouter.delete('/:placeId/wish', ensureAuthenticated, asyncHandler(async (req, res) => {
     const { placeId } = req.params;
     const { userId } = req.body;
     await prisma.wish.deleteMany({ // many가 안전하기는 할 듯.. 다른데서 확실히 1대1 보장받는거 아니면
@@ -116,53 +152,5 @@ router.delete('/:placeId/wish', asyncHandler(async (req, res) => {
     });
     res.status(200).json({message: "성공적으로 삭제되었습니다."});
 }));
-/////////////////////////////////////////////////////////////////////////////////테스트완
-
-// 여행지 찜 수 조회
-router.get('/:placeId/wishCount', asyncHandler(async (req, res) => {
-    const { placeId } = req.params;
-    const count = await prisma.wish.count({
-        where: {placeId}
-    });
-    res.status(200).json({count});
-}));
-
-// 여행지 리뷰 등록
-router.post('/:placeId/reviews', asyncHandler(async (req, res) => {
-    const { placeId } = req.params;
-    const { userId, comment } = req.body;
-    const newWish = await prisma.wish.create({
-        data: {
-            userId,
-            placeId,
-            comment
-        }
-    });
-    res.status(201).json(newWish);
-}));
-
-// 여행지 찜 삭제
-router.delete('/:placeId/wish', asyncHandler(async (req, res) => {
-    const { placeId } = req.params;
-    const { userId } = req.body;
-    await prisma.wish.deleteMany({ // many가 안전하기는 할 듯.. 다른데서 확실히 1대1 보장받는거 아니면
-        where: {
-            userId,
-            placeId // 순서 안중요
-        }
-    });
-    res.status(200).json({message: "성공적으로 삭제되었습니다."});
-}));
-
-
-// 여행지 찜 수 조회
-router.get('/:placeId/wishCount', asyncHandler(async (req, res) => {
-    const { placeId } = req.params;
-    const count = await prisma.wish.count({
-        where: {placeId}
-    });
-    res.status(200).json({count});
-}));
-
 
 export default router;
